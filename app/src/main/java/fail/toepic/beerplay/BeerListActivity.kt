b19711c81fd51a5a46c50c70c6c28231d6ab5b33
windwishers <shadowog@gmail.com>
2018-12-09 15:17:07 +0900

    프로젝트 초기화

---
+++ app/src/main/java/fail/toepic/beerplay/BeerListActivity.kt
@@ -0,0 +1,12 @@
+package fail.toepic.beerplay
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BeerListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beer_list)
+    }
+}
